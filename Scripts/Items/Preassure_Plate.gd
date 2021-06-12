7cb4352aaa04afd700b1b3fa47a83bc91b175f44
mio991 <mio991@mio991.de>
2021-06-12 22:58:29 +0200

    added preassure plate signal

--- Scripts/Items/Preassure_Plate.gd
+++ Scripts/Items/Preassure_Plate.gd
@@ -1,9 +1,14 @@
 extends Area2D
 
+signal change_state(enabled)
+
 func _on_Area2D_body_entered(body):
-	$Preassure_Plate.frame = 1
+	if $Preassure_Plate.frame != 1:
+		$Preassure_Plate.frame = 1
+		emit_signal("change_state", true)
 
 
 func _on_Area2D_body_exited(body):
 	if get_overlapping_bodies().size() == 0:
 		$Preassure_Plate.frame = 0
+		emit_signal("change_state", false)
