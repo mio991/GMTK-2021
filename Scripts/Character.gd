7f87ecbc384788130833d37a39f4a6c5adac84ad
mio991 <mio991@mio991.de>
2021-06-12 10:36:40 +0200

    Basic Tileset and Character Movement

---
+++ Scripts/Character.gd
@@ -0,0 +1,24 @@
+extends KinematicBody2D
+
+export(float) var speed = 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta):
+	var direction = Vector2(0,0)
+	if Input.is_action_pressed("move_right"):
+		direction += Vector2(1,0)
+	if Input.is_action_pressed("move_left"):
+		direction += Vector2(-1,0)
+	if Input.is_action_pressed("move_up"):
+		direction += Vector2(0,-1)
+	if Input.is_action_pressed("move_down"):
+		direction += Vector2(0,1)
+	
+	direction = direction.clamped(1)
+	
+	self.move_and_slide(direction * self.speed * delta)
