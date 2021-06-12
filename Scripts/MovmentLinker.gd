6d8a309d4418326856775d59f702d1ab31e91e65
mio991 <mio991@mio991.de>
2021-06-12 12:55:23 +0200

    Add Rigidbody Ball

---
+++ Scripts/MovmentLinker.gd
@@ -0,0 +1,17 @@
+extends Node
+
+func _ready():
+	for child in self.get_children():
+		if child.has_signal("interacted_with"):
+			child.connect("interacted_with", self, "try_link")
+
+func try_link():
+	var bodies = Array()
+	for child in self.get_children():
+		if child is Area2D:
+			for body in child.get_overlapping_bodies():
+				if body.get_collision_layer_bit(1):
+					bodies.append(body)
+	
+	if bodies.size() >= 2:
+		print_debug(bodies)
