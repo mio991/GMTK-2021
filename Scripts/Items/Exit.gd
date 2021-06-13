5fc0df084070fd8371b3420297641bfba4181ce7
mio991 <mio991@mio991.de>
2021-06-13 16:30:00 +0200

    exists are a thing

--- Scripts/Items/Exit.gd
+++ Scripts/Items/Exit.gd
@@ -1,11 +1,27 @@
-extends Node2D
+extends Area2D
+
+export(String, "red", "blue") var type = "red"
 
 var active:bool = false
 
-func state_changed(active:bool):
-	self.active = active
+func _ready():
+	print_debug(type)
+	$AnimatedSprite.animation = type
 
-func _on_Exit_interacted_with():
-	if active:
-		var container = get_tree().current_scene
-		container.load_level()
+func state_changed(state:bool):
+	if state:
+		$AnimatedSprite.play(type)
+	else:
+		$AnimatedSprite.play(type, true)
+		self.active = false
+
+
+func _on_AnimatedSprite_animation_finished():
+	self.active = true
+	if !get_overlapping_bodies().empty():
+		get_tree().current_scene.load_level()
+
+
+func _on_Exit_body_entered(body):
+	if self.active:
+		get_tree().current_scene.load_level()
--- Scripts/Temperature/SystemManager.gd
+++ Scripts/Temperature/SystemManager.gd
@@ -3,7 +3,6 @@ extends Node
 var needs_update = Array()
 
 func _process(delta):
-	print_debug(self.needs_update)
 	for obj in self.needs_update:
 		obj.apply_changes()
 	
