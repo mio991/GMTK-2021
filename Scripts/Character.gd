97516b8f75b3a9f2196f1d5df6e1b5930783f42e
mio991 <mio991@mio991.de>
2021-07-31 09:30:31 +0200

    Character movment reworked to tile based movement

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -1,16 +1,54 @@
-extends KinematicBody2D
+extends Area2D
 
+class_name Character
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+export(NodePath) var sprite = "./Sprite"
 
+onready var sprite_node = get_node(sprite)
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var next_movment:Vector2 = Vector2.ZERO
 
+func _unhandled_input(event):
+	if event.is_action_pressed("move_left"):
+		next_movment = Vector2.LEFT
+	if event.is_action_pressed("move_right"):
+		next_movment = Vector2.RIGHT
+	if event.is_action_pressed("move_up"):
+		next_movment = Vector2.UP
+	if event.is_action_pressed("move_down"):
+		next_movment = Vector2.DOWN
+	
+	if !$Tween.is_active():
+		next_movment = try_move(next_movment)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func try_move(move:Vector2):
+	if sprite_node == null:
+		return move
+	
+	$Ray.set_cast_to(move * 32)
+	$Ray.force_raycast_update()
+	if $Ray.is_colliding():
+		return move
+	$Tween.interpolate_property(self, "position", position, position + move * 32, 1, Tween.TRANS_LINEAR)
+	$Tween.start()
+	
+	match move:
+		Vector2.LEFT:
+			sprite_node.animation = "left"
+		Vector2.RIGHT:
+			sprite_node.animation = "right"
+		Vector2.UP:
+			sprite_node.animation = "up"
+		Vector2.DOWN:
+			sprite_node.animation = "down"
+		Vector2.ZERO:
+			sprite_node.animation = "default"
+			
+	return Vector2.ZERO
+
+func _on_tween_completed():
+	next_movment = try_move(next_movment)
+
+func _process(delta):
+	if !$Tween.is_active():
+		next_movment = try_move(next_movment)
--- Scripts/CharacterController.gd
+++ Scripts/CharacterController.gd
@@ -1,5 +1,14 @@
 extends Node
 
-func _input(event):
-	
+var characters = {};
+
+func _ready():
+	get_tree().connect("node_added", self, "_on_node_added");
+
+func _on_node_added(node:Node):
+	if node is Character:
+		characters[node.type] = node;
+
+func _on_node_removed(node:Node):
+	characters.values().has()
 	pass
